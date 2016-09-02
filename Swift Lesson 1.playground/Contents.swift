6d502cfa0e31dbc414c1212c283ac05e8bc95602
Andrew Barber <edwardbar423@gmail.com>
2016-09-02 10:53:01 -0400

    This is my Swift Lesson 1 file for playground

---
+++ Swift Lesson 1.playground/Contents.swift
@@ -0,0 +1,38 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+765
+
++
+
+55
+
+32 * 2
+
+15/5
+
+100053 - 1453
+
+(2000 + 50 - 800) / 4
+
+// 34 + 56 + 230
+
+200 + 34 + 45
+
+//I'm editing this existing expression, because I want you to know that I know how to comment on things. <3.
+
+//----------------------------
+
+//This is an error code mentioned in as an example.
+1000 / 0
+
+//This is me using a code to fix the example.
+
+1000 / 5 //<- There are no more errors!
+
+
+
+
+
+
